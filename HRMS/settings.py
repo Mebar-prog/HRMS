6d6e72385dd69ec6071dbfcfb26a1d9120d4a710
Mebar-Prog <sherabwang111@gmail.com>
2023-06-06 18:12:21 +0600

    app line changed

--- HRMS/settings.py
+++ HRMS/settings.py
@@ -36,6 +36,8 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
+    'api',
+    'frontend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,8 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'api',
-    'frontend',
     'rest_framework',
     'rest_framework_simplejwt',
 ]
